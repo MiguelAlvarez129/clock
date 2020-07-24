4ff80489df260c20a9a11b6c0e08049fc427c199
Miguel Alvarez <miguelalvarezunexpo@gmail.com>
2020-07-23 23:02:39 -0400

    Updates

---
+++ precache-manifest.39a9a854b293d75687b3a6e1eaa98839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635159fd6fc59a932c159a6c3232e00d",
+    "url": "/clock/index.html"
+  },
+  {
+    "revision": "f747621903160191e290",
+    "url": "/clock/static/css/main.9f5e1a73.chunk.css"
+  },
+  {
+    "revision": "bcf9cb057fa712d663fe",
+    "url": "/clock/static/js/2.66d0bb46.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clock/static/js/2.66d0bb46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f747621903160191e290",
+    "url": "/clock/static/js/main.82f32767.chunk.js"
+  },
+  {
+    "revision": "9fbb0c64d7d7588fb297",
+    "url": "/clock/static/js/runtime-main.bf40dc10.js"
+  }
+]);
